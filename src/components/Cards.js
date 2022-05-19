0550fc81cd4fc19f2a6943dd2cb662efaf5568ba
roberromero <robertoromerodeveloper@gmail.com>
2022-05-19 17:29:00 +0100

    add

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,15 +6,16 @@ function Cards({ data }) {
 
   const datos= data;
   const handleDelete = (e) => {
-      console.log(e);
-  
+      const elemento = e.target.parentElement.id
+    console.log(elemento);      
+    // ESTOY A PUNTO................
   }
 
   return (
     <div className='containerCards'>
        {
          datos.map(item=>{
-           return <div className='containerCard' value={item['id']} key={item['id']}>
+           return <div className='containerCard' value={item['id']} key={item['id']} id={item['id']}>
                     <p><b>Task:</b> {item['Task']}</p>
                     <p><b>Task description:</b> {item['TaskDescription']}</p>
                     <button onClick={handleDelete}>Delete</button>
